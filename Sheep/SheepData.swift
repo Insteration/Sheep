51165df488ee6c44446da5f55fc048e4a9ef7c86
Artem <inste@icloud.com>
2019-06-06 14:26:34 +0300

    Second commit

---
+++ Sheep/SheepData.swift
@@ -0,0 +1,18 @@
+//
+//  SheepData.swift
+//  Sheep
+//
+//  Created by Artem Karmaz on 06.06.2019.
+//  Copyright © 2019 Artem Karmaz. All rights reserved.
+//
+
+let dataSheep = [Sheep(id: 0, name: "Francheska", imageName: "sheep1", description: "Cheerful little sheep."),
+                 Sheep(id: 1, name: "Polly", imageName: "sheep2", description: "Brave and funny sheep."),
+                 Sheep(id: 2, name: "Brenda", imageName: "sheep3", description: "Little and good-natured sheep."),
+                 Sheep(id: 3, name: "Mike", imageName: "sheep4", description: "Clever and sad sheep."),
+                 Sheep(id: 4, name: "Tereza", imageName: "sheep5", description: "Merciful and good-natured sheep."),
+                 Sheep(id: 5, name: "Nina", imageName: "sheep6", description: "Brave and beautiful sheep."),
+                 Sheep(id: 6, name: "Woody", imageName: "sheep7", description: "Mr. Silence in sheep world."),
+                 Sheep(id: 7, name: "Spector", imageName: "sheep8", description: "Winter and style sheep.."),
+                 Sheep(id: 8, name: "Frosty", imageName: "sheep9", description: "Cool guy sheep."),
+                 Sheep(id: 9, name: "Mike", imageName: "sheep10", description: "Gangsta sheep.")]
